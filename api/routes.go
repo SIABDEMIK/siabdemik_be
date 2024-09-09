2e26003ef30491a17e3373b66f9c7d3f6fcbbe18
Ahmad Rifki Ayala <ayalarifki@gmail.com>
2024-09-09 16:15:19 +0700

    update routes

--- api/routes.go
+++ api/routes.go
@@ -5,6 +5,7 @@ import (
 
 	handlers "Ayala-Crea/server-app-absensi/api/handlers"
 	"Ayala-Crea/server-app-absensi/api/handlers/data"
+	"Ayala-Crea/server-app-absensi/api/handlers/role"
 
 	"github.com/gorilla/mux"
 )
@@ -14,8 +15,12 @@ func AllRoutes(r *mux.Router, db *sql.DB) {
 	r.HandleFunc("/register", handlers.RegisterUser(db)).Methods("POST")
 	r.HandleFunc("/login", handlers.Login(db)).Methods("POST")
 
+	// data karyawan and mahasiswa
 	r.HandleFunc("/upload", data.UploadExcel(db)).Methods("POST")
 	r.HandleFunc("/data", data.GetAllStudentsEmployees(db)).Methods("GET")
 	r.HandleFunc("/data/mahasiswa", data.GetDataByIdAdmin(db)).Methods("GET")
 	r.HandleFunc("/data/input", data.CreateDataManual(db)).Methods("POST")
+
+	// role
+	r.HandleFunc("/insert/role", role.InsertRole(db)).Methods("POST")
 }
